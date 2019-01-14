78e89b2cb0b2eafd45ab30d9b67d2fb51a5a5031
Venkatesh Mankena <mankenavenkatesh@gmail.com>
2019-01-14 16:10:03 +0530

    Update RegistrationContract.sol

--- contracts/RegistrationContract.sol
+++ contracts/RegistrationContract.sol
@@ -1,53 +1,56 @@
 pragma solidity ^0.5.1;
-contract RegistrationContract {
-    
-    mapping(address => address[]) collegeStudentsRegistration;
-    mapping(address => address[]) studentCollegesRegistration;
-    
-    
-    enum Stages {
-        AcceptingRegistrationRequests,
-        RequestforRegistration,
-        VerifyStudentProfile,
-        ApproveRegistration,
-        AcceptRegistration
-    }
-    
-    Stages public stage = Stages.AcceptingRegistrationRequests;
-    uint public creationTime = now;
-    
-     modifier atStage(Stages _stage) {
-        require(stage == _stage);
-        _;
-    }
-     // This modifier goes to the next stage
-    // after the function is done.
-    modifier transitionNext()
-    {
-        _;
-        nextStage();
-    }
-    function nextStage() internal {
-        stage = Stages(uint(stage) + 1);
-    }
-    
-    function requestForRegistration() public atStage(Stages.AcceptingRegistrationRequests) transitionNext
-    {
+    contract RegistrationContract {
         
-    }
-    
-    function verifyStudentProfile() public atStage(Stages.RequestforRegistration) transitionNext
-    {
+        address studentAddress;
+        address collegeAddress;
         
-    }
-    
-    function approveRegistration() public atStage(Stages.VerifyStudentProfile) transitionNext
-    {
+        enum Stages {
+            AcceptingRegistrationRequests,
+            RequestforRegistration,
+            VerifyStudentProfile,
+            ApproveRegistration,
+            AcceptRegistration
+        }
         
-    }
-    
-    function acceptRegistration() public atStage(Stages.ApproveRegistration) transitionNext
-    {
+        Stages public stage = Stages.AcceptingRegistrationRequests;
+        uint public creationTime = now;
         
+         modifier atStage(Stages _stage) {
+            require(stage == _stage);
+            _;
+        }
+         // This modifier goes to the next stage
+        // after the function is done.
+        modifier transitionNext()
+        {
+            _;
+            nextStage();
+        }
+        function nextStage() internal {
+            stage = Stages(uint(stage) + 1);
+        }
+        
+        function getRegistrationStatus() public view returns (string) {
+            return state;
+        }
+        
+        function requestForRegistration() public atStage(Stages.AcceptingRegistrationRequests) transitionNext
+        {
+            
+        }
+        
+        function verifyStudentProfile() public atStage(Stages.RequestforRegistration) transitionNext
+        {
+            
+        }
+        
+        function approveRegistration() public atStage(Stages.VerifyStudentProfile) transitionNext
+        {
+            
+        }
+        
+        function acceptRegistration() public atStage(Stages.ApproveRegistration) transitionNext
+        {
+            
+        }
     }
-}
