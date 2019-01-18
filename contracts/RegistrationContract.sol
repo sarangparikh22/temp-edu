9daa09c9d3e40607ea2cc80b7831ae29f29f933c
Venkatesh Mankena <mankenavenkatesh@gmail.com>
2019-01-18 11:25:58 +0530

    Update RegistrationContract.sol

--- contracts/RegistrationContract.sol
+++ contracts/RegistrationContract.sol
@@ -1,64 +1,74 @@
 pragma solidity ^0.5.1;
     contract RegistrationContract {
-        
         address studentAddress;
         address collegeAddress;
         
         struct StudentInfo {
-            string collegeRegId;
+            string collegeRegNumber;
             string collegeEmailId;
-            uint dateOfJoining;
-            uint dateOfPassing;
+            uint collegeDoJ;
+            uint collegeDateOfPassing;
         }
         
-        enum Stages {
-            RequestforRegistration,
-            VerifyStudentProfile,
-            ApproveRegistration,
-            AcceptRegistration
-        }
-        
-        Stages public stage = Stages.RequestforRegistration;
-        uint public creationTime = now;
-        
-         modifier atStage(Stages _stage) {
-            require(stage == _stage);
-            _;
-        }
-         // This modifier goes to the next stage
-        // after the function is done.
-        modifier transitionNext()
-        {
-            _;
-            nextStage();
-        }
-        function nextStage() internal {
-            stage = Stages(uint(stage) + 1);
-        }
-        
-        constructor(address _studentAddress, address _collegeAddress, string collegeRegNumber, string collegeEmailId, uint collegeDoJ, uint collegeDateOfPassing){
+        StudentInfo studentInfo;
+         
+          constructor(address _studentAddress, address _collegeAddress, string memory collegeRegNumber, string memory collegeEmailId, uint collegeDoJ, uint collegeDateOfPassing) public{
             studentAddress = _studentAddress;
             collegeAddress = _collegeAddress;
-            studentInfo = new studentInfo(collegeRegNumber, collegeEmailId, collegeDoJ, collegeDateOfPassing);
-            
+            studentInfo = StudentInfo(collegeRegNumber, collegeEmailId, collegeDoJ, collegeDateOfPassing);
+                        
         }
         
-        function getRegistrationStatus() public view returns (string) {
-            return state;
-        }
-        
-        function verifyStudentProfile() public atStage(Stages.RequestforRegistration) transitionNext
-        {
+        //   constructor(address _studentAddress, address _collegeAddress, string collegeRegNumber, string collegeEmailId, uint collegeDoJ, uint collegeDateOfPassing){
+        //     studentAddress = _studentAddress;
+        //     collegeAddress = _collegeAddress;
+        //     studentInfo = new studentInfo(collegeRegNumber, collegeEmailId, collegeDoJ, collegeDateOfPassing);
             
-        }
+        // }
         
-        function approveRegistration() public atStage(Stages.VerifyStudentProfile) transitionNext
-        {
-            
-        }
+        // enum Stages {
+        //     RequestforRegistration,
+        //     VerifyStudentProfile,
+        //     ApproveRegistration,
+        //     AcceptRegistration
+        // }
         
-        function acceptRegistration() public atStage(Stages.ApproveRegistration) transitionNext
-        {
+        // Stages public stage = Stages.RequestforRegistration;
+        // uint public creationTime = now;
+        
+        //  modifier atStage(Stages _stage) {
+        //     require(stage == _stage);
+        //     _;
+        // }
+        //  // This modifier goes to the next stage
+        // // after the function is done.
+        // modifier transitionNext()
+        // {
+        //     _;
+        //     nextStage();
+        // }
+        // function nextStage() internal {
+        //     stage = Stages(uint(stage) + 1);
+        // }
+        
+      
+        
+        // function getRegistrationStatus() public view returns (string) {
+        //     return state;
+        // }
+        
+        // function verifyStudentProfile() public atStage(Stages.RequestforRegistration) transitionNext
+        // {
             
-        }
+        // }
+        
+        // function approveRegistration() public atStage(Stages.VerifyStudentProfile) transitionNext
+        // {
+            
+        // }
+        
+        // function acceptRegistration() public atStage(Stages.ApproveRegistration) transitionNext
+        // {
+            
+        // }
     }
