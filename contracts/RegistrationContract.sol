9dfcc11b66798dc642c56e6b5c5b0b1936e96905
Venkatesh Mankena <mankenavenkatesh@gmail.com>
2019-01-18 11:51:51 +0530

    Update RegistrationContract.sol

--- contracts/RegistrationContract.sol
+++ contracts/RegistrationContract.sol
@@ -16,59 +16,73 @@ pragma solidity ^0.5.1;
             studentAddress = _studentAddress;
             collegeAddress = _collegeAddress;
             studentInfo = StudentInfo(collegeRegNumber, collegeEmailId, collegeDoJ, collegeDateOfPassing);
-                        
         }
         
-        //   constructor(address _studentAddress, address _collegeAddress, string collegeRegNumber, string collegeEmailId, uint collegeDoJ, uint collegeDateOfPassing){
-        //     studentAddress = _studentAddress;
-        //     collegeAddress = _collegeAddress;
-        //     studentInfo = new studentInfo(collegeRegNumber, collegeEmailId, collegeDoJ, collegeDateOfPassing);
-            
-        // }
         
-        // enum Stages {
-        //     RequestforRegistration,
-        //     VerifyStudentProfile,
-        //     ApproveRegistration,
-        //     AcceptRegistration
-        // }
+        enum Stages {
+            RequestforRegistration,
+            VerifyStudentProfile,
+            ApproveRegistration,
+            AcceptRegistration
+        }
         
-        // Stages public stage = Stages.RequestforRegistration;
-        // uint public creationTime = now;
+        Stages public stage = Stages.RequestforRegistration;
+        uint public creationTime = now;
         
-        //  modifier atStage(Stages _stage) {
-        //     require(stage == _stage);
-        //     _;
-        // }
+         modifier atStage(Stages _stage) {
+            require(stage == _stage);
+            _;
+        }
         //  // This modifier goes to the next stage
         // // after the function is done.
-        // modifier transitionNext()
-        // {
-        //     _;
-        //     nextStage();
-        // }
-        // function nextStage() internal {
-        //     stage = Stages(uint(stage) + 1);
-        // }
+        modifier transitionNext()
+        {
+            _;
+            nextStage();
+        }
+        function nextStage() internal {
+            stage = Stages(uint(stage) + 1);
+        }
         
       
+      modifier isStudent(address addr) {
+          require(addr == studentAddress);
+          _;
+      }
+      
+      modifier isCollege(address addr) {
+          require(addr == collegeAddress);
+          _;
+      }
         
-        // function getRegistrationStatus() public view returns (string) {
-        //     return state;
-        // }
+        function getRegistrationStatus() public view returns (string memory) {
+            if(Stages.RequestforRegistration == stage){
+                return "RequestforRegistration";
+            }
+            if(Stages.VerifyStudentProfile == stage){
+                return "VerifyStudentProfile";
+            }
+            if(Stages.ApproveRegistration == stage){
+                return "ApproveRegistration";
+            }
+            if(Stages.AcceptRegistration == stage){
+                return "AcceptRegistration";
+            }
+        }
         
-        // function verifyStudentProfile() public atStage(Stages.RequestforRegistration) transitionNext
-        // {
+        
+        function verifyStudentProfile() public atStage(Stages.RequestforRegistration) isCollege(msg.sender) transitionNext
+        {
             
-        // }
+        }
         
-        // function approveRegistration() public atStage(Stages.VerifyStudentProfile) transitionNext
-        // {
+        function approveRegistration() public atStage(Stages.VerifyStudentProfile) isCollege(msg.sender) transitionNext
+        {
             
-        // }
+        }
         
-        // function acceptRegistration() public atStage(Stages.ApproveRegistration) transitionNext
-        // {
+        function acceptRegistration() public atStage(Stages.ApproveRegistration) isStudent(msg.sender) transitionNext
+        {
             
-        // }
+        }
     }
